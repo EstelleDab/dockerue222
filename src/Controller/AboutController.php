c118159df81a44aaed0c737ed2d6a9d85da0eb99
EstelleDab <152979607+EstelleDab@users.noreply.github.com>
2024-01-28 04:49:34 +0100

    ajout des fichiers cachés
    gitignore env et git

--- src/Controller/AboutController.php
+++ src/Controller/AboutController.php
@@ -6,13 +6,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class BlogController extends AbstractController
+class AboutController extends AbstractController
 {
-    #[Route('/', name: 'blog')]
+    #[Route('/about', name: 'app_about')]
     public function index(): Response
     {
-        return $this->render('blog/index.html.twig', [
-            'controller_name' => 'Baptiste'
+        return $this->render('about/index.html.twig', [
+            'controller_name' => 'Estelle, an aspiring dev needing an aspirin'
         ]);
     }
 }
